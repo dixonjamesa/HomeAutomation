d19d460141bc72ed7da306965dbfd8e59d124b35
dixonjamesa <dixonjamesa@gmail.com>
2016-01-30 23:03:10 +0000

    Major memory saving and Node2 as multi-sensor
    Turns out strings go into SRAM. Doh!
    Node2 code changed to be a multi sensor - DHT22, light and PIR

--- HAS_Node2/HAS_Node2.ino
+++ HAS_Node2/HAS_Node2.ino
@@ -3,39 +3,44 @@
 #include <SPI.h>
 #include <HACommon.h>
 #include <HAHelper.h>
-#include <CapacitiveSensor.h>
+#include <DHT.h>
 
 // PIN Configuration:
 
-const byte senseTx = 3;
-const byte senseRx = 4;
-const byte senseFb = 8;
 const byte RFCE = 9; // transmitter
 const byte RFCSN = 10; // transmitter
-const byte outputPin = 2;
+const byte outputPin1 = 6;
+const byte outputPin2 = 7;
+const byte LDRPin = A7;
+const byte motionPin = 2;
+const byte switchPin = 5;
+const byte tempPin = 3;
 
 // Constants that identify this node and the node to send data to
-const uint16_t this_node = 011;
+const uint16_t this_node = 021;
 const uint16_t control_node = 0;
 
-// the touch sensor
-CapacitiveSensor CSensor = CapacitiveSensor(senseTx,senseRx);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
+#define DHTTYPE 22
+DHT dht(tempPin, DHTTYPE);
 
-//initialisation state
-bool allInitialised=false;
+byte lightSensor;
+bool motionSensor;
+float temperature;
+float humidity;
+
+bool allInitialised = false;
 
 // Switch states
 bool latchState=false;
 bool toggleState=false;
 bool resendsetup=false;
-bool dummy=false;
 
 // Radio with CE & CSN pins
 RF24 radio(RFCE, RFCSN);
 RF24Network RFNetwork(radio);
 
 // Time between checks (in ms)
-const unsigned long interval = 200;
+const unsigned long loopTime = 200;
 
 class MyHANet: public HomeAutoNetwork
 {
@@ -45,58 +50,67 @@ class MyHANet: public HomeAutoNetwork
   {
     switch( message->code )
     {
-      case 201: // monitor channel:
-        // attach our output pin to this channel...
-        Serial.print("Been told to monitor channel ");
-        Serial.println(message->data);
-        SubscribeChannel( DT_BOOL, outputPin, message->data );
-        RegisterChannel( DT_BOOL, 101, &dummy, 1, message->data, false ); // so we can send messages on this code
-        allInitialised = true;
-      break;
       case 202: // reset
-        // all we need to do is re-register the channels...
-        Serial.println("Reset");
-        InitialiseMessaging(true);
+        Serial.println(F("Reset"));
       break;
-      case outputPin:
+      case outputPin1:
+      case outputPin2:
         toggleState = (bool)message->data[0];
-        Serial.print("Data received from node ");
+        Serial.print(F("Data received from node "));
         Serial.print(from_node);
         // Check value and change the pin...
-        Serial.print(" {Code: ");
+        Serial.print(F(" {Code: "));
         Serial.print(message->code);
-        Serial.print(", Value: ");
+        Serial.print(F(", Value: "));
         Serial.print(toggleState);
-        Serial.println("}");
-        digitalWrite(outputPin, toggleState);
+        Serial.println(F("}"));
+        digitalWrite(message->code, toggleState);
       break;
       default:
-        Serial.print("Received unexpected code ");
+        Serial.print(F("Received unexpected code "));
         Serial.println(message->code);
       break;
     }
   }
+  
+  // when we receive a MSG_UNKNOWN code (with data payload)
   virtual void OnUnknown(uint16_t from_node, message_data *_message) 
   {
     // let's at least report the problem...
-    Serial.print("Message returned UNKNOWN: ");
+    Serial.print(F("Message returned UNKNOWN: "));
     Serial.println(_message->code);
+    if( allInitialised)
+    {
+      // assume it's because of lost signal, so just reset ourselves...
+      InitialiseMessaging(true);
+    }
   }
+
+  // Controller has told us to reset
   virtual void OnResetNeeded()
   {
     // all we need to do is re-register the channels...
-    Serial.println("RESET");
+    Serial.println(F("RESET"));
     InitialiseMessaging(true);
   }
-  void InitialiseMessaging(bool _restart)
+  
+  void InitialiseMessaging(bool _restart=false)
   {
     char channel[64];
-    sprintf(channel, "home/node%o/monitor", this_node);
-    SubscribeChannel( DT_TEXT, 201, channel); // we will be told what channel to do switching for
-    sprintf(channel, "home/node%o/reset", this_node);
+    sprintf(channel, "n%o/status", this_node);
+    RegisterChannel( DT_TEXT, 101, StatusMessage, channel, _restart); // common status reporting method
+    strcpy(StatusMessage, "Initialising...");
+    sprintf(channel, "n%o/reset", this_node);
     SubscribeChannel( DT_TEXT, 202, channel); // we will be told to reset here
-    sprintf(channel, "home/node%o/sendsetup", this_node);
-    RegisterChannel( DT_BOOL, 200, &resendsetup, 0, channel, false); // use this to prompt a further wake message to get initialisation info
+
+    sprintf(channel, "n%o/brightness", this_node);
+    RegisterChannel( DT_BYTE, 102, &lightSensor, 5, channel, _restart); 
+    sprintf(channel, "n%o/motion", this_node);
+    RegisterChannel( DT_BOOL, 103, &motionSensor, 0, channel, _restart); 
+    sprintf(channel, "n%o/temp", this_node);
+    RegisterChannel( DT_FLOAT, 104, &temperature, 0.1f, channel, _restart);
+    sprintf(channel, "n%o/humid", this_node);
+    RegisterChannel( DT_FLOAT, 105, &humidity, 1, channel, _restart);
     allInitialised = false;
   }
 
@@ -115,62 +129,52 @@ void setup(void)
   delay(5);
   RFNetwork.begin(120, this_node);
   radio.setRetries(8,11);
-  RFNetwork.txTimeout = 529;
+  RFNetwork.txTimeout = 537;
   
   HANetwork.Begin(this_node);
 
-  pinMode(outputPin, OUTPUT);
-  pinMode(senseFb, OUTPUT);
-  CSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  pinMode(outputPin1, OUTPUT);
+  pinMode(outputPin2, OUTPUT);
+  pinMode(LDRPin, INPUT);
+  pinMode(motionPin, INPUT);
+  pinMode(switchPin, INPUT_PULLUP);
 
   delay(50);
 
-  HANetwork.InitialiseMessaging(false);
-  Serial.println("Initialised");
+  HANetwork.InitialiseMessaging();
+  Serial.println(F("Initialised"));
 }
 
 void loop() 
 {
-  long total;  
   // Update network data
   RFNetwork.update();
-  HANetwork.Update(interval);
-  
-  total =  CSensor.capacitiveSensor(30);
-  Latch(total);
-  //Serial.println(total);
+  HANetwork.Update(loopTime);
+
+  if( !allInitialised && HANetwork.QueueEmpty() ) 
+  {
+    allInitialised = true;
+    strcpy(StatusMessage, "OK.");
+  }
+
+  // read our data pins...
+  int raw = analogRead(LDRPin);
+  raw -= 300; if( raw<0) raw=0;
+  raw = raw/6; if (raw>100) raw=100;
+  lightSensor = raw; // scaling through looking at readings.
+  motionSensor = digitalRead(motionPin);
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  digitalWrite(outputPin1, motionSensor);
+/*
+  Serial.print(temperature);
+  Serial.print(F("deg, "));
+  Serial.print(humidity);
+  Serial.print(F("hum, "));
+  Serial.print(motionSensor);
+  Serial.print(F("MO, "));
+  Serial.println(lightSensor);*/
   // Wait a bit before we start over again
-  delay(interval);
-
-  if( !allInitialised )
-  {
-    delay(400);
-    toggleState = !toggleState;
-    digitalWrite(outputPin, toggleState);
-    resendsetup = !resendsetup; // trigger a message
-  }
-}
-
-long ThresholdHigh = 200;
-long ThresholdLow = 120;
-void Latch(long value)
-{
-  if(latchState == false && value > ThresholdHigh)
-  {
-    toggleState = !toggleState;
-    digitalWrite(outputPin, toggleState);
-    message_data msg;
-    msg.code = 101;
-    msg.type = DT_BOOL;
-    memcpy(msg.data, &toggleState, 1);
-    HANetwork.SendMessage(&msg, 1);
-    latchState = true;
-  }
-  else if( latchState == true && value < ThresholdLow )
-  {
-    latchState = false;
-  }
-
-  digitalWrite(senseFb, latchState);
+  delay(loopTime);
 }
 
